7b49555341a9c46687723fb6a19fa0bc3c299b91
David Malone <dmalone@pivotal.io>
2016-01-23 09:09:53 -0500

    added Godeps, Cloud Foundry application manifest, and modified main.go to use an env var called PORT to drive which port the server binds to

--- main.go
+++ main.go
@@ -1,8 +1,20 @@
 package main
 
-import "github.com/dave-malone/trec/services"
+import (
+	"fmt"
+	"os"
+
+	"github.com/dave-malone/trec/services"
+)
 
 func main() {
+	port := os.Getenv("PORT")
+	if len(port) == 0 {
+		port = "3000"
+	}
+
+	fmt.Printf("Running server on port %v\n", port)
+
 	m := trec.NewServer()
-	m.Run()
+	m.RunOnAddr(":" + port)
 }
